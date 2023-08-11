f22c93c464d6bb32ed73868a15046604fda193ee
yusrA312 <yusra2yahya@gmail.com>
2023-08-11 09:13:51 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,13 +5,13 @@
     * Return: Always 0 (Success)
      */
 int main(void)
-{int i;
+{int n;
 for (n = 48; n < 58; n++)
-{ putchar(n);
-if (n != 57) }
+{putchar(n);
+if (n != 57)
 { putchar(',');
 putchar(' '); }
 }
 putchar('\n');
 return (0);
-}	
+}
