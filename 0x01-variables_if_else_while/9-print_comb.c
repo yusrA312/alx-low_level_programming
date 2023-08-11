97ff5c6627ba0ee8f5618cff327e42baf81d80ec
yusrA312 <yusra2yahya@gmail.com>
2023-08-11 08:54:44 -0700

    clang

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+   * main - prints all possible combinations of single-digit numbers
+    * Return: Always 0 (Success)
+     */
+int main(void)
+{int i;
+for (n = 48; n < 58; n++)
+{ putchar(n);
+if (n != 57) }
+{ putchar(',');
+putchar(' '); }
+}
+putchar('\n');
+return (0);
+}	
