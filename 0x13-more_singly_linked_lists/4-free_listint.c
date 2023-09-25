31f9a182209b34f340784c46d2a23ef3f39541a8
yusrA312 <yusra2yahya@gmail.com>
2023-09-25 13:40:18 -0700

    done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tem;
+
+	while (head)
+	{
+		tem = head->next;
+		free(head);
+		head = tem;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tem;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tem = (*head)->next;
+		free(*head);
+		*head = tem;
+	}
+
+	*head = NULL;
+}
