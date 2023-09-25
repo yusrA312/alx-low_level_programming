38c53c8c5b118c87e253d4c95f745bf399eb6986
yusrA312 <yusra2yahya@gmail.com>
2023-09-25 15:51:04 -0700

    done

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,9 +9,11 @@
 int sum_listint(listint_t *head)
 {
 	int i;
-	int sum =  0 ;
+	int sum =  0;
 	listint_t *ptr = head;
 
+	if (!ptr)
+		return (0);
 	for (i = 0 ; ptr ; i++)
 	{sum += ptr->n;
 	ptr = ptr->next;
