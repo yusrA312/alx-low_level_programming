7f372f7cea3d1fb7101199e3eb11ec7dfb2fca63
yusrA312 <yusra2yahya@gmail.com>
2023-09-25 11:47:59 -0700

    dond

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,23 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
+
 {
 	int i = 0;
+
 	if (h == NULL)
 		printf("The list is empty");
 	else
-		for (i=0; h != NULL ;)
+		for (i = 0; h;)
 		{
-			  i++;
+			i++;
 			h = h->next;
-		
 		}
 
 	return (i);
