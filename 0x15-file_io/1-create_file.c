fdc06cd5b2a69d1bf4cd00638e7851b25ffe35aa
yusrA312 <yusra2yahya@gmail.com>
2023-10-02 11:22:33 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
 /**
-*create_file-Create a file with specified name and write
+ * create_file - Create a file with specified name.
  *
  * @filename: The name of the file to be created.
- * @text_content: The text content to write to the file
+ * @text_content: The text content to write to the file.
  *
- * Return: 1 on success, -1 on failure .
+ * Return: 1 on success, -1 on failure.
  *         The created file has permissions: rw-------.
- * If the file already exists, it will be truncated to contain the new content.
- *         If filename is NULL, return -1.
+ * If the file already exists, it will be truncated .
+ * If filename is NULL, return -1.
  *         If text_content is NULL, create an empty file.
  */
-
 int create_file(const char *filename, char *text_content)
-
 {
-
-	int f;
-	int x;
-
+	int w, len = 0, p;
 
 	if (filename == NULL)
-
 		return (-1);
 
-	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	x = write(f, text_content, strlen(text_content));
+	p = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-
-	if (f == -1 || x == -1)
-
+	if (p == -1)
 		return (-1);
 
+	if (text_content != NULL)
+	{		len = strlen(text_content);
 
-	close(f);
+		w = write(p, text_content, len);
+	}
 
+	if (w == -1)
+		return (-1);
+
+	close(p);
 
 	return (1);
-
 }
