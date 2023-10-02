af94dd6a17e93bd85f2ac874b1cd96009f222c9a
yusrA312 <yusra2yahya@gmail.com>
2023-10-02 10:53:27 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 {
 
-	int f, x, l = 0;
+	int f, x;
 
 
 	if (filename == NULL)
@@ -30,10 +30,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	x = write(fd, text_content, strlen(text_content));
+	x = write(f, text_content, strlen(text_content));
 
 
-	if (fd == -1 || x == -1)
+	if (f == -1 || x == -1)
 
 		return (-1);
 
