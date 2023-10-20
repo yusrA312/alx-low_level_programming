9428fe0afc229afe4317a8614bc6a980e4c48886
yusrA312 <yusra2yahya@gmail.com>
2023-10-20 12:14:19 -0700

    done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _endianness (void);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
