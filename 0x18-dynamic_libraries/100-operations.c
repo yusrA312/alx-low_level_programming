10babe56450931736ffaffb6a65931af6a4eed6a
yusrA312 <yusra2yahya@gmail.com>
2023-12-10 22:37:56 -0800

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	if (b == 0) 
+	{ printf("Erorr: Division by zero\n");
+		return (0); }
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	if (b == 0)
+	{ printf("Erorr: Division by zero\n");
+		return (0); }
+	return (a % b);
+}
