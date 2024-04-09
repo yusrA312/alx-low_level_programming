cab60201913f37e7cfc2e828f89260246b7e839d
yusrA312 <yusra2yahya@gmail.com>
2024-04-09 05:10:39 -0700

    done

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Function that performs a linear search
+ * to find the target value
+ * @array: The passed in array
+ * @size: The size of the given array
+ * @value: The target value to be searched for
+ * Return: The index of the target value, or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int Yy = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	/*
+	 * Size is typecasted into 
+	 */
+	while (Yy < (int)size)
+	{
+		printf("Value checked array[%d] = [%d]\n", Yy, array[Yy]);
+		if (array[Yy] == value)
+			return (Yy);
+
+		Yy++;
+	}
+	return (-1);
+}
