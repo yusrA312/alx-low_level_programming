91350eac6348c7eda0b21d995cd2d11b90bdac32
yusrA312 <yusra2yahya@gmail.com>
2024-04-09 07:43:25 -0700

    done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,53 +1,54 @@
 #include "search_algos.h"
 
 /**
- * get_midpoint - Function that calculates the midpoint
- * @begin: The starting index of the array segment
+ * calculate_midpoint - Function that calculates the midpoint
+ * @start: The starting index of the array segment
  * @end: The ending index of the array segment
  * Return: The calculated midpoint
  */
-int get_midpoint(int begin, int end)
+int calculate_midpoint(int start, int end)
 {
-	return (begin + (end - begin) / 2);
+	return (start + (end - start) / 2);
 }
 
 /**
  * binary_search - Function that performs a binary search
  * to find the target value
- * @array: The passed in array
+ * @arr: The passed in array
  * @size: The size of the given array
- * @value: The target value to be searched for
+ * @target: The target value to be searched for
  * Return: The index of the target value, or -1 if not found
  */
-int binary_search(int *array, size_t size, int value)
+int binary_search(int *arr, size_t size, int target)
 {
-	int begin = 0, end = (int)size - 1;
-	int mid = 0, temp = 0;
+	int begin = 0, last = (int)size - 1;
+	int mid = 0, tmp = 0;
 
-	if (array == NULL)
+	if (arr == NULL)
 		return (-1);
 
-	while (begin <= end)
+	while (begin <= last)
 	{
-		temp = begin;
+		tmp = begin;
 		printf("Searching in array: ");
-		while (temp <= end)
+		while (tmp <= last)
 		{
-			printf("%d", array[temp]);
-			if (temp != end)
+			printf("%d", arr[tmp]);
+			if (tmp != last)
 				printf(", ");
-			temp++;
+			tmp++;
 		}
 		printf("\n");
 
-		mid = get_midpoint(begin, end);
-		if (array[mid] == value)
+		mid = calculate_midpoint(begin, last);
+		if (arr[mid] == target)
 			return (mid);
-		else if (array[mid] < value)
+		else if (arr[mid] < target)
 			begin = mid + 1;
 		else
-			end = mid - 1;
+			last = mid - 1;
 	}
 
 	return (-1);
 }
+
