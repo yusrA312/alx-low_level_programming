6f4e6141ae6f3eb0d6ff2a3a18cb69cb9053a242
yusrA312 <yusra2yahya@gmail.com>
2024-04-10 05:43:50 -0700

    done

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Searches for a value in a sorted array
+ *                      of integers using exponential search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the index where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the array.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t start, end, mid, i;
+
+	if (array == NULL)
+		return (-1);
+
+	start = 0;
+	end = size - 1;
+
+	if (array[start] == value)
+		return (start);
+
+	for (i = 1; i < size && array[i] <= value; i *= 2)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, end);
+
+	while (end >= i / 2)
+	{
+		printf("Searching in array: ");
+		for (size_t j = i / 2; j < end; j++)
+			printf("%d, ", array[j]);
+		printf("%d\n", array[end]);
+
+		mid = i / 2 + (end - i / 2) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+			end = mid - 1;
+		else
+			start = mid + 1;
+	}
+
+	return (-1);
+}
+
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int linear_search_range(int *array, size_t start, size_t end, int value);
 int get_midpoint(int begin, int end);
 size_t calculate_pos(size_t l, size_t h, int v, int *arr);
+int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 #endif
